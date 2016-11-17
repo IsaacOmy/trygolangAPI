57bd3966d43a80734ac9136932b7b34c0b86d02e
IsaacOmy <isaac.omy@gmail.com>
2016-11-17 13:24:36 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/IsaacOmy/Project-LearningByDoing/muxAPI/Handle"
+)
+
+// Existing code from above
+
+func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	Handle.HandleRequests()
+}
